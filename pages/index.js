f34cb8006f3b3b9598b9ddc041a43a13ed7bf23b
Name <tabrisg@gmail.com>
2023-12-06 21:16:19 +0100

    AAAAA

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="I like turtles!" />
+        <Header title="I like turtlesssss!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
